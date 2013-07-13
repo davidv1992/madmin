a1f8b0f7ee135fb77181a4d653e8faada418926c
David Venhoek <davidv1992@msn.com>
2013-07-13 11:03:04 +0200

    Implemented policy for madmin_product

---
+++ policy/madmin_product.py
@@ -0,0 +1,11 @@
+# Product type constants
+product_type_kantine = 1 # Required
+
+# Product type description mapping
+product_name_mapping = {
+	product_type_kantine: 'kantine'
+}
+
+product_id_mapping = {
+	'kantine': product_type_kantine
+}
