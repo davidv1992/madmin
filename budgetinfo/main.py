de48f8dadea2c218e36199bac2cc1aa54bed8596
David Venhoek <davidv1992@msn.com>
2013-11-02 22:28:52 +0100

    Modified budgetinfo to both streamline the interface Also added list all capabilities

--- budgetinfo/main.py
+++ budgetinfo/main.py
@@ -12,7 +12,7 @@ cmd_parser = argparse.ArgumentParser(description='Vraag budgetinformatie op voor
 cmd_parser.add_argument(
 	'verenigingen', 
 	metavar='vereniging', 
-	type=str, nargs='+', 
+	type=str, nargs='*', 
 	help='Vereniging waarover informatie verlangt wordt.'
 )
 cmd_parser.add_argument(
@@ -21,6 +21,12 @@ cmd_parser.add_argument(
 	help='Gebruikersnaam om mee in te loggen (standaard de gebruikersnaam waarme is ingelogd op de lokale machine).',
 	default=getpass.getuser()
 )
+cmd_parser.add_argument(
+	'--all', '-a',
+	action='store_true',
+	default=False,
+	help='Geef informatie van alle verenigingen.'
+)
 
 arguments = cmd_parser.parse_args()
 
@@ -28,6 +34,7 @@ arguments = cmd_parser.parse_args()
 password = getpass.getpass()
 try:
 	servercall.login(arguments.user, password)
+	verenigingen = servercall.remote_call('/verenigingen')
 except servercall.ServerCallException:
 	print >> sys.stderr, 'Kon geen verbinding opbouwen met madmin server.'
 	sys.exit(1)
@@ -35,11 +42,24 @@ except servercall.ServerLoginError:
 	print >> sys.stderr, 'Kon niet inloggen met gegeven gebruikersnaam/wachtwoord.'
 	sys.exit(1)
 
-#Retrieve vereniging information
-verenigingen = servercall.remote_call('/verenigingen')
+if arguments.all:
+	for vereniging in verenigingen:
+		try:
+			budgetten = servercall.remote_call('/budget/ver', [('vereniging_id', vereniging['id'])])
+		except servercall.ServerCallException:
+			print >> sys.stderr, 'Kon geen verbinding opbouwen met madmin server'
+			sys.exit(1)
+		print '{0}:'.format(vereniging['naam'])
+		for budget in budgetten:
+			current_euro = budget['current']/100
+			current_cent = budget['current']%100
+			minimum_euro = budget['minimum']/100
+			minimum_cent = budget['minimum']%100
+			print '{0}: {1}.{2:02} (Minimum: {3}.{4:02})'.format(budget['naam'], current_euro, current_cent, minimum_euro, minimum_cent)
 
 for vereniging in arguments.verenigingen:
 	selection = filter(lambda x: x['naam'] == vereniging, verenigingen)
+	
 	if len(selection) == 0:
 		print >> sys.stderr, 'Vereniging {0} bestaat niet.'.format(vereniging)
 		continue
@@ -54,4 +74,4 @@ for vereniging in arguments.verenigingen:
 		current_cent = budget['current']%100
 		minimum_euro = budget['minimum']/100
 		minimum_cent = budget['minimum']%100
-		print '{0}: {1}.{2:02} (Minimum: {3}.{4:02})'.format(current_euro, current_cent, minimum_euro, minimum_cent)
+		print '{0}: {1}.{2:02} (Minimum: {3}.{4:02})'.format(budget['naam'], current_euro, current_cent, minimum_euro, minimum_cent)
