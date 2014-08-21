ce1bfbc3bf5e81774b45d9e768026b522c69d024
David Venhoek <david@venhoek.nl>
2014-08-21 09:47:42 +0200

    Added all product types

--- server/policy/madmin_product.py
+++ server/policy/madmin_product.py
@@ -1,11 +1,17 @@
 # Product type constants
 product_type_kantine = 1 # Required
+product_type_borrel = 2
+product_type_emballage = 3
 
 # Product type description mapping
 product_name_mapping = {
-	product_type_kantine: 'kantine'
+	product_type_kantine: 'kantine',
+	product_type_borrel: 'borrel',
+	product_type_emballage: 'emballage',
 }
 
 product_id_mapping = {
-	'kantine': product_type_kantine
+	'kantine': product_type_kantine,
+	'borrel': product_type_borrel,
+	'emballage': product_type_emballage,
 }
