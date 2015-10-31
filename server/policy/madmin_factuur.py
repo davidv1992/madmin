d79b2e099c44c209a35c0fd73b45314a2c77182f
mrngm <mrngm@moeilijklastig.nl>
2015-10-31 18:21:12 +0100

    madmin_factuur: fix filename infoleak, fixes #23

--- server/policy/madmin_factuur.py
+++ server/policy/madmin_factuur.py
@@ -175,7 +175,8 @@ def process_factuur(factuur, fac_id):
 	pdfFile = open(pdfFilename, "rb")
 	pdfAttachment = MIMEApplication(pdfFile.read())
 	pdfFile.close()
-	pdfAttachment.add_header('Content-Disposition', 'attachment', filename=pdfFilename)
+	attachmentFilename = info['vereniging'] + " - " + info['factuurdatum'] + " - " + info['factuurnummer'] + ".pdf"
+	pdfAttachment.add_header('Content-Disposition', 'attachment', filename=attachmentFilename)
 	
 	emailContentTemplate = R"""Dit is een automatisch gegenereerde mail met de factuur %(vereniging)s %(factuurnummer)s, %(verantwoordelijke)s."""
 	emailSubjectTemplate = R"""Madmin factuur %(vereniging)s %(factuurnummer)s"""
